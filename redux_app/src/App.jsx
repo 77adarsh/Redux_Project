7d878a6038972fc3c0707e955708a8dfd80453cd
Adarsh Chaudhary <144239960+77adarsh@users.noreply.github.com>
2024-12-28 22:52:59 +0530

    Partially completion of AddPostForm

--- redux_app/src/App.jsx
+++ redux_app/src/App.jsx
@@ -1,3 +1,4 @@
+import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div>
         <PostsList />
+        <AddPostForm />
       </div>
     </>
   )
--- redux_app/src/features/posts/postsSlice.jsx
+++ redux_app/src/features/posts/postsSlice.jsx
@@ -8,9 +8,17 @@ const initialState = [
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducers: {}
+    reducers: {
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload);
+            }
+        }
+    }
 })
 
 export const selectAllPosts = (state) => state.posts
+
+export const { postAdded, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
