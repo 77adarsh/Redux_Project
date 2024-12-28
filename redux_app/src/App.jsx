de66482ffaa2ce03b70ad99395ab20f403a1a70b
Adarsh Chaudhary <144239960+77adarsh@users.noreply.github.com>
2024-12-28 20:54:53 +0530

    first commit

---
+++ redux_app/src/App.jsx
@@ -0,0 +1,14 @@
+import PostsList from "./features/posts/PostsList";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <PostsList />
+      </div>
+    </>
+  )
+}
+
+export default App
